2e2f17e09490874a6038469d369ff3fe8ac81b1b
Tom Kotula <test-git@youcode.pl>
2023-11-16 12:28:57 +0100

    adding new tasks and showing if a task is done

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
         console.log("Welcome everyone searching for a new adventures with HTML, CSS and JS!")
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
     const tasks = [
         {
             content: "Nagrać lekcję",
@@ -17,9 +25,9 @@
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks) {
+        for (const task of tasks) {
             htmlString += `
-                <li>
+                <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
                 ${task.content}
                 </li>
             `;
@@ -28,9 +36,25 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    }
+
     const init = () => {
         render();
         welcome();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
