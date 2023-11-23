0a41d23f28af72d220cb274b7f9d51bcb10b97ef
Tom Kotula <test-git@youcode.pl>
2023-11-23 15:49:12 +0100

    formatting code

--- script.js
+++ script.js
@@ -10,14 +10,14 @@
 
         render();
     };
-    
+
     const removeTask = (taskIndex) => {
         tasks.splice(taskIndex, 1);
         render();
-    } 
+    }
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = ! tasks[taskIndex].done;
+        tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     };
 
